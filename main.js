929eb9da156a1f49e4bbbc7bca5d1223b5ff7d8b
CosminBozai <cosminbozai@outlook.com>
2022-09-23 13:33:30 +0100

    Display weather

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const APIKey = "a7bd0b44b3f09e778ab9d3192bcc0217";
+const submitLocationBtn = document.getElementById("submit-location");
+submitLocationBtn.addEventListener("click", getWeather);
 
 async function getGeoLocation(location) {
   let response = await fetch(
@@ -9,7 +11,8 @@ async function getGeoLocation(location) {
   return responseData;
 }
 
-async function getWeatherData(location) {
+async function getWeather() {
+  let location = document.getElementById("location-input").value;
   let geoLocation = await getGeoLocation(location);
   let response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${geoLocation[0].lat}&lon=${geoLocation[0].lon}&units=metric&appid=${APIKey}`
@@ -18,10 +21,15 @@ async function getWeatherData(location) {
   //   console.log(unprocessedData);
   let weatherData = {
     weather: unprocessedData.weather[0].main,
-    temp: unprocessedData.main.temp,
+    temperature: unprocessedData.main.temp,
     feelsLike: unprocessedData.main.feels_like,
     humidity: unprocessedData.main.humidity,
     windSpeed: unprocessedData.wind.speed,
   };
-  console.log(weatherData);
+  displayWeather(weatherData);
+}
+
+function displayWeather(data) {
+  const temperatureSpan = document.querySelector("p span");
+  temperatureSpan.textContent = data.temperature;
 }
