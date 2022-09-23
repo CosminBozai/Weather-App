11b6e4841c6bc39b1e471fd170a188d03cd64896
CosminBozai <cosminbozai@outlook.com>
2022-09-23 15:36:24 +0100

    Refractoring with Weather module and IIFEs
    Added Weather module
    The functions are now more self-sufficient and new functions can be added without changing the others
    Made geoLocation an IIFE
    geoLocation's value is used in the other functions
    Made getWeather an IIFE
    Added weatherForecast(at the moment just with unprocessed data)

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
-const APIKey = "a7bd0b44b3f09e778ab9d3192bcc0217";
 const submitLocationBtn = document.getElementById("submit-location");
-submitLocationBtn.addEventListener("click", getWeather);
+const Weather = () => {
+  const APIKey = "a7bd0b44b3f09e778ab9d3192bcc0217";
 
-async function getGeoLocation(location) {
-  let response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${APIKey}`,
-    { mode: "cors" }
-  );
-  let responseData = await response.json();
-  return responseData;
-}
+  const geoLocation = (async function getGeoLocation() {
+    let location = document.getElementById("location-input").value;
+    let response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${APIKey}`,
+      { mode: "cors" }
+    );
+    let responseData = await response.json();
+    return responseData;
+  })();
 
-async function getWeather() {
-  let location = document.getElementById("location-input").value;
-  let geoLocation = await getGeoLocation(location);
-  let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${geoLocation[0].lat}&lon=${geoLocation[0].lon}&units=metric&appid=${APIKey}`
-  );
-  let unprocessedData = await response.json();
-  //   console.log(unprocessedData);
-  let weatherData = {
-    weather: unprocessedData.weather[0].main,
-    temperature: unprocessedData.main.temp,
-    feelsLike: unprocessedData.main.feels_like,
-    humidity: unprocessedData.main.humidity,
-    windSpeed: unprocessedData.wind.speed,
-  };
-  displayWeather(weatherData);
-}
+  (async function getWeather() {
+    let coord = await geoLocation;
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0].lat}&lon=${coord[0].lon}&units=metric&appid=${APIKey}`
+    );
+    let unprocessedData = await response.json();
+    let weatherData = {
+      weather: unprocessedData.weather[0].main,
+      temperature: unprocessedData.main.temp,
+      feelsLike: unprocessedData.main.feels_like,
+      humidity: unprocessedData.main.humidity,
+      windSpeed: unprocessedData.wind.speed,
+    };
+    displayWeather(weatherData);
+  })();
 
-function displayWeather(data) {
-  const temperatureSpan = document.querySelector("p span");
-  temperatureSpan.textContent = data.temperature;
-}
+  function displayWeather(data) {
+    const temperatureSpan = document.querySelector("p span");
+    temperatureSpan.textContent = data.temperature;
+  }
+
+  (async function getForecast() {
+    let coord = await geoLocation;
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${coord[0].lat}&lon=${coord[0].lon}&appid=${APIKey}`
+    );
+    let unprocessedData = await response.json();
+    console.log(unprocessedData);
+  })();
+};
+
+submitLocationBtn.addEventListener("click", Weather);
