7a28a9b2113d183073fcbd853862e7b74b3a6f6e
CosminBozai <cosminbozai@outlook.com>
2022-09-23 19:03:04 +0100

    Added dom for displaying the data

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const Weather = () => {
 
   const geoLocation = (async function getGeoLocation() {
     let location = document.getElementById("location-input").value;
-    let response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${APIKey}`,
-      { mode: "cors" }
-    );
-    let responseData = await response.json();
-    return responseData;
+    try {
+      let response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${APIKey}`,
+        { mode: "cors" }
+      );
+      let responseData = await response.json();
+      return responseData;
+    } catch (err) {
+      alert(err);
+    }
   })();
 
   (async function getWeather() {
@@ -18,7 +22,9 @@ const Weather = () => {
       `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0].lat}&lon=${coord[0].lon}&units=metric&appid=${APIKey}`
     );
     let unprocessedData = await response.json();
+    console.log(unprocessedData);
     let weatherData = {
+      locationName: unprocessedData.name,
       weather: unprocessedData.weather[0].main,
       temperature: unprocessedData.main.temp,
       feelsLike: unprocessedData.main.feels_like,
@@ -29,8 +35,18 @@ const Weather = () => {
   })();
 
   function displayWeather(data) {
-    const temperatureSpan = document.querySelector("p span");
-    temperatureSpan.textContent = data.temperature;
+    let headerDisplay = document.querySelector("h1 span");
+    headerDisplay.textContent = data.locationName;
+    let weatherDisplay = document.getElementById("weather");
+    weatherDisplay.textContent = data.weather;
+    let temperatureDisplay = document.getElementById("temperature");
+    temperatureDisplay.textContent = Math.round(data.temperature) + "°C";
+    let feelsLikeDisplay = document.querySelector("#feels-like span");
+    feelsLikeDisplay.textContent = Math.round(data.feelsLike) + "°C";
+    let humidityDisplay = document.querySelector("#humidity span");
+    humidityDisplay.textContent = data.humidity + "%";
+    let windSpeedDisplay = document.querySelector("#wind span");
+    windSpeedDisplay.textContent = data.windSpeed + "km/h";
   }
 
   (async function getForecast() {
@@ -39,7 +55,6 @@ const Weather = () => {
       `https://api.openweathermap.org/data/2.5/forecast?lat=${coord[0].lat}&lon=${coord[0].lon}&appid=${APIKey}`
     );
     let unprocessedData = await response.json();
-    console.log(unprocessedData);
   })();
 };
 
